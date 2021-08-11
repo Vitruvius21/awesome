da043aa06c0d9af6eae0e788981c31e4da816d71
Vitruvius21 <balakhadze.dev@gmail.com>
2021-08-12 03:40:20 +0400

    init 2

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'ewesome';
+
+  constructor() {}
+
+  method(): void {
+    if (confirm('This will hack your PC, proceed?') === true) {
+      window.location.href = 'https://twitter.com/so_ewesome';
+    } else {
+      alert('I was kidding :D');
+    }
+  }
+}
