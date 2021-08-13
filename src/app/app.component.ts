d7f01dc9ea4d15e24efa6ccade9f0d0a11c1cad1
ᚷᛁᛟᚱᚷᛁ ᛒᚨᛚᚨᚲᚻᚨᛞᛉᛖ <balakhadze.dev@gmail.com>
2021-08-13 13:35:27 +0400

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'ewesome';
+  title = 'awesome';
 
   constructor() {}
 
