9b194c6340bd482138262467019c807c0fc0395a
ᚷᛁᛟᚱᚷᛁ ᛒᚨᛚᚨᚲᚻᚨᛞᛉᛖ <balakhadze.dev@gmail.com>
2021-08-13 13:42:42 +0400

    update

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ewesome'`, () => {
+  it(`should have as title 'awesome'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ewesome');
+    expect(app.title).toEqual('awesome');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ewesome app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('awesome app is running!');
   });
 });
